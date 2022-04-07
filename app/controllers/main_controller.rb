5d757a3296fe1d8fd00210509a7c45743154e970
sjosephk1975@gmail.com <sjosephk1975@gmail.com>
2022-04-07 06:41:54 +0100

    Updated Gemfile.lock

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,5 @@
 class MainController < ApplicationController
   def index
-    flash[:notice] = "Logged in succesfully"
-    flash[:alert] = "Invalid email or password"
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,27 @@
 # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
   get "about", to: "about#index", as: :about
 
+  get "password", to: "password#edit", as: :edit_password
+  patch "password", to: "password#update"
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+
+  get "password/reset", to: "password_reset#new"
+  post "password/reset", to: "password_reset#create"
+  get "password/reset/edit", to: "password_reset#new"
+  patch "password/reset/edit", to: "password_reset#update"
+
+  get "/auth/twitter/callback", to: "omniauth_callbacks#twitter"
+
+  resources :twitter_accounts
+  resources :tweets
   root to: "main#index"
 end
